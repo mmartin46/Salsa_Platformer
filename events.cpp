80612d4cadb3db8921c41e2bdd9ef1fe2bc64387
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-05-30 13:45:31 -0500

    Changed main.hpp

---
+++ events.cpp
@@ -0,0 +1,86 @@
+#include "main.hpp"
+
+
+int processEvents(SDL_Window *window, GameState *gameState)
+{
+    SDL_Event event;
+    int done = 0;
+
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+            case SDL_WINDOWEVENT_CLOSE:
+            {
+                if (window)
+                {
+                    SDL_DestroyWindow(window);
+                    window = NULL;
+                    done = 1;
+                }
+            }
+            break;
+            case SDL_KEYDOWN:
+            {
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        done = 1;
+                    break;
+                    case SDLK_UP:
+                        if (gameState->plyr.get_onBlock())
+                        {
+                            gameState->plyr.set_dy(PLAYER_JUMP_HEIGHT);
+                            gameState->plyr.reset_onBlock();
+                        }
+                }
+            }
+            break;
+            case SDL_QUIT:
+                // quit out of the game
+                done = 1;
+            break;
+        }
+    }
+
+    // More jumping
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_UP])
+    {
+        gameState->plyr.apply_jump();
+    }
+
+    // Walking
+    if (state[SDL_SCANCODE_LEFT])
+    {
+       gameState->plyr.move_left_x();
+       if (gameState->plyr.get_dx() < -6)
+       {
+           gameState->plyr.move_left_dx(); 
+       }
+       gameState->plyr.facingLeft = 1;
+       gameState->plyr.slowingDown = 0;
+    }
+    else if (state[SDL_SCANCODE_RIGHT])
+    {
+       gameState->plyr.move_right_x();;
+       if (gameState->plyr.get_dx() > 6)
+       {
+           gameState->plyr.move_right_dx();
+       }
+       gameState->plyr.facingLeft = 0;
+       gameState->plyr.slowingDown = 0;
+    }
+    else
+    {
+        // Slows down to 0.
+        gameState->plyr.animFrame = 0;
+        gameState->plyr.slow_movement();
+        gameState->plyr.slowingDown = 1;
+        if (SDL_fabsf(gameState->plyr.get_dx()) < 0.1f)
+        {
+            gameState->plyr.apply_static_movement();
+        }
+    }
+    return done;
+}
---
+++ graphics.cpp
@@ -0,0 +1,90 @@
+#include "main.hpp"
+
+void loadGame(GameState *gameState)
+{
+    SDL_Surface *surface = NULL;
+    
+    // Load images and create rending textures from them
+    // Loading the image's first frame.
+    surface = IMG_Load("plyr_ita.png");
+    if (surface == NULL)
+    {
+        printf("Cannot find plyr_ita.png!\n\n");
+        SDL_Quit();
+        exit(1);
+    }
+    gameState->plyrFrames[0] = SDL_CreateTextureFromSurface(gameState->renderer, surface);
+    SDL_FreeSurface(surface);
+
+    // Loading the image's second frame.
+    surface = IMG_Load("plyr_itb.png");
+    if (surface == NULL)
+    {
+        printf("Cannot find plyr_itb.png!\n\n");
+        SDL_Quit();
+        exit(1);
+    }
+    gameState->plyrFrames[1] = SDL_CreateTextureFromSurface(gameState->renderer, surface);
+    SDL_FreeSurface(surface);
+
+    surface = IMG_Load("block.png");
+    if (surface == NULL)
+    {
+        printf("Cannot find block.png!\n\n");
+        SDL_Quit();
+        exit(1);
+    }
+    gameState->block = SDL_CreateTextureFromSurface(gameState->renderer, surface);
+    SDL_FreeSurface(surface);
+
+
+
+    gameState->plyr.set_x(210-PLAYER_WIDTH);    
+    gameState->plyr.set_y(240-PLAYER_HEIGHT);
+    gameState->plyr.set_dx(0);
+    gameState->plyr.set_dy(0);
+    gameState->plyr.reset_onBlock();
+    gameState->plyr.animFrame = 0;
+    gameState->plyr.facingLeft = 1;
+    gameState->plyr.slowingDown = 0;
+
+    gameState->set_time(0);
+    gameState->scrollX = 0;
+
+
+    // init blocks
+    for (int i = 0; i < 100; ++i)
+    {
+        gameState->blocks.at(i).w = 60;           
+        gameState->blocks.at(i).h = 20;            
+        gameState->blocks.at(i).x = i*60;         
+        gameState->blocks.at(i).y = 400;                
+    }
+}
+
+
+
+void doRender(SDL_Renderer *renderer, GameState *gameState)
+{
+    // set the drawing color to blue
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    // clear the screen (to blue)
+    SDL_RenderClear(renderer);
+
+    // set the drawing color to white
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    for (int i = 0; i < 100; ++i)
+    {
+        SDL_Rect ledgeRect = { (int) (gameState->scrollX + gameState->blocks[i].x), gameState->blocks[i].y, gameState->blocks[i].w, gameState->blocks[i].h };
+        SDL_RenderCopy(renderer, gameState->block, NULL, &ledgeRect);
+    }
+
+    // draw a rectangle at plyr's position
+    SDL_Rect rect = { (int) (gameState->scrollX + gameState->plyr.get_x()), (int)gameState->plyr.get_y(), PLAYER_WIDTH, PLAYER_HEIGHT };
+    SDL_RenderCopyEx(renderer, gameState->plyrFrames[gameState->plyr.animFrame], NULL, &rect, 0, NULL, (SDL_RendererFlip)(gameState->plyr.facingLeft == 0));
+
+
+    SDL_RenderPresent(renderer);    
+}
