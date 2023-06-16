7eeced3491215552690668a74c370becc36e62ed
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-06-16 00:34:31 -0500

    New level

--- game.cpp
+++ game.cpp
@@ -271,8 +271,8 @@ void GameState::init_blocks(int level_choice)
     }
     else if (choice >= 40)
     {
-        this->get_backdrop()->set_x(-50);
-        this->get_backdrop()->set_y(-50);   
+        this->get_backdrop()->set_x(-30);
+        this->get_backdrop()->set_y(-30);   
         this->get_backdrop()->set_h(437);
         this->get_backdrop()->set_w(700); 
     }
