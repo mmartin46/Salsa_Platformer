fc370ae0a84b4db644b8165eb589f0192da29edf
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-06-18 22:26:11 -0500

    Refactoring

--- utilities.cpp
+++ utilities.cpp
@@ -1,5 +1,6 @@
 #include "utilities.hpp"
-// Returns the minimum in a vector.
+
+
 double minimum(const vector<double> &v)
 {
     double min = v.at(0);
@@ -14,17 +15,12 @@ double minimum(const vector<double> &v)
     return min;
 }
 
-// Are two rectangles colliding.
 int collide2d(float x1, float y1, float x2, float y2, float ht1, float wt1, float wt2, float ht2)
 {
     return (!((x1 > (x2+wt2)) || (x2 > (x1+wt1)) || (y1 > (y2+ht2)) || (y2 > (y1+ht1))));
 }
 
-// Allows the user to load a texture and returns a surface.
-//
-// Parameters:
-// \param file includes the file path
-// \param error_msg includes an error message if the file isn't found.
+
 SDL_Surface* get_surface(const char *file, const char *error_msg)
 {
     SDL_Surface *surface = NULL;
@@ -39,7 +35,6 @@ SDL_Surface* get_surface(const char *file, const char *error_msg)
     return surface;   
 }
 
-// Uses the manhattan distance formula
 double get_distances(double x_1, double x_2, double y_1, double y_2)
 {
     double val = sqrt(pow(x_1 - x_2, 2) + pow(y_1 - y_2, 2));
