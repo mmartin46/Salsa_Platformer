2cd2bb2e8f8f52d0e0b63fe5780ddd7774bb8e02
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-06-30 11:19:00 -0500

    Added Changes

--- game.hpp
+++ game.hpp
@@ -3,10 +3,10 @@
 #define GAME_H
 
 
-#include "src\include\SDL2\SDL.h"
-#include "src\include\SDL2\SDL_image.h"
-#include "src\include\SDL2\SDL_mixer.h"
-#include "src\include\SDL2\SDL_ttf.h"
+#include <SDL2\SDL.h>
+#include <SDL2\SDL_image.h>
+#include <SDL2\SDL_mixer.h>
+#include <SDL2\SDL_ttf.h>
 #include "cmath"
 #include "utilities.cpp"
 
